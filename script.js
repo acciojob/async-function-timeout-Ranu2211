d465c33546facfef2a8d562a9d05e1123224b178
Ranu <88536226+Ranu2211@users.noreply.github.com>
2025-01-02 16:49:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+document.getElementById('use-form').addEventListener('click', async function(e){
+	e.preventDefault();
+	const text = document.getElementById('text').value.trim();
+	const delay = parseInt(document.getElementById('delay').value.trim(),10);
+	if(!text || isNaN(delay) || delay<=0){
+		alert("plase provide valid values");
+		return;
+	}
+	 function lateInTime(){
+		 return new Promise((resolve, reject) => {
+			 setTimeout(()=>{
+			resolve(text);
+		}, delay);
+		 });
+	}
+	 
+		const result = await lateInTime();
+		document.getElementById('output').textContent = result;
+});
